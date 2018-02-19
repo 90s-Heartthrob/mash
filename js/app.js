4605cd182c88de9efd69ccaa609f27341e39df96
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-19 14:47:59 -0800

    added event listener

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-crush = ['crush1', 'crush2', 'crush3']
-location = ['location1', 'location2', 'location3']
-car = ['uInput1', 'uInput2', 'uInput3',]
-job = ['uInput1', 'uInput2', 'uInput3']
+'use strict';
+
+// const crush = ['crush1', 'crush2', 'crush3'];
+// const location = ['location1', 'location2', 'location3'];
+// const car = ['uInput1', 'uInput2', 'uInput3',];
+// const job = ['uInput1', 'uInput2', 'uInput3'];
 
 
 const gameObj = {
@@ -11,18 +13,21 @@ const gameObj = {
     job: [],
     Tally: 0,
 
-    start: function (){
+    start: function () {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', function() {
+            event.preventDefault();
+            const crush = this.crush1.value;
+            const location = this.location1.value;
+            const car = this.car1.value;
+            const job = this.job1.value;
+            console.log(crush, location, car, job);
+        
+        });
+    },
 
-    }
-}
+};
 
-const form = document.querySelector('form');
-form.addEventListener('submit', function() {
-    event.preventDefault();
-    const crush = this.crush1.value;
-    const location = this.location1.value;
-    const car = this.car1.value;
-    const job = this.job1.value;
-console.log(crush, location, car, job)
 
-});
+gameObj.start();
+
