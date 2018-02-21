435572c71c4da867cb638f6aff2a8680de1b5a0b
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-21 12:58:37 -0800

    button displays past story

--- js/home.js
+++ js/home.js
@@ -1,11 +1,16 @@
 'use strict'
 
 if (localStorage.getItem('pastStory')) {
-    const pastStory = JSON.parse(localStorage.getItem('pastStory'));
-    console.log(pastStory);
     const storySection = document.getElementById('past-story');
     const button = document.createElement('button')
     button.setAttribute('type', button)
-    button.textContent = 'Click Here!';
+    button.textContent = 'Click Here to read your past story!';
     storySection.appendChild(button);
+    button.addEventListener('click', function() {
+        const pastStory = JSON.parse(localStorage.getItem('pastStory'));
+        console.log(pastStory);
+        const p = document.createElement('p');
+        p.textContent = pastStory;
+        storySection.appendChild(p);
+    })
 }
