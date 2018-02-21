989dfd9e84cd10e8fe212bf1c37b8d75e4a9ef6b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-21 10:30:47 -0800

    updated the populate function with multiple story options

--- js/story.js
+++ js/story.js
@@ -15,26 +15,41 @@ const story = {
         this.house = JSON.parse(localStorage.getItem('houseData'));
     },
 
-    getRandomInt: function(max = 3) {
-        const randomNumer = Math.floor(Math.random() * Math.floor(max));
-        console.log(randomNumer);
+    getRandomInt: function() {
+        const min = Math.ceil(1);
+        const max = Math.floor(3);
+        const randomNumer = Math.floor(Math.random() * (max - min + 1)) + min;;
         return randomNumer;
     },
 
     populate: function () {
-        // const num = this.getRandomInt();
+        story.getData();
+
+        const num = this.getRandomInt();
+        console.log(num);
+
         const story1 = document.getElementById('story');
         const p = document.createElement('p');
-        const summaryOne = 'I guess you chose ' + this.crush + '. Which is cool. Not so bad as you live in a ' + this.house + ' in ' + this.location + '. Have a functional & sensible ' + this.car + '. You also work as a ' + this.job + '.';
-        // const summaryTwo = 'OH NO!! You married ' + this.crush + '. You baught & live in a' + this.house + ' in ' + this.location + '. Also have to drive a ' + this.car + ', to your damn job as a ' + this.job + '.';
-        // const summaryThree = 'Oh yeah you end up with ' + this.crush + '. When you\'re not working you spend you time at home in a ' + this.house + ' in ' + this.location + '. Also get to drive your ' + this.car + '. Which is the best way to get to work where you\'re a ' + this.job + '.';
-        p.textContent = summaryOne;
-        story1.appendChild(p);
+
+        if (num === 1) {
+            const summaryOne = 'I guess you chose ' + this.crush + '. Which is cool. Not so bad as you live in a ' + this.house + ' in ' + this.location + '. Have a functional & sensible ' + this.car + '. You also work as a ' + this.job + '.';
+            p.textContent = summaryOne;
+            story1.appendChild(p);
+        } else if (num === 2) {
+            const summaryTwo = 'OH NO!! You married ' + this.crush + '. You baught & live in a' + this.house + ' in ' + this.location + '. Also have to drive a ' + this.car + ', to your damn job as a ' + this.job + '.';
+            p.textContent = summaryTwo;
+            story1.appendChild(p);
+        } else {
+            const summaryThree = 'Oh yeah you end up with ' + this.crush + '. When you\'re not working you spend you time at home in a ' + this.house + ' in ' + this.location + '. Also get to drive your ' + this.car + '. Which is the best way to get to work where you\'re a ' + this.job + '.';
+            p.textContent = summaryThree;
+            story1.appendChild(p);
+        }
     }
+};
 
+story.populate();
 
-
-
+// key = pastStory
 
 
 
@@ -51,9 +66,6 @@ const story = {
 //     this.style.color = 'red'
 // } )
 
-};
 
-story.getData();
-story.populate();
 
 
